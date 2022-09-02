b8e5e7e47357208562778435a00d6fd52d5d0e1b
deep-logic2000 <deeplogic2000@gmail.com>
2022-09-02 10:26:43 +0300

    Added locales test file

--- src/store/actionCreators/timerAC.js
+++ src/store/actionCreators/timerAC.js
@@ -4,6 +4,7 @@ import {
   REMOVE_TIMER_FROM_QUEUE,
   ADD_CURRENT_LOG,
   CLEAR_LOGS,
+  SET_CURRENT_TIMER,
 } from "../actions/timerActions";
 
 export const addTimerToQueue = (timerDelay) => (dispatch, getState) => {
@@ -11,36 +12,34 @@ export const addTimerToQueue = (timerDelay) => (dispatch, getState) => {
   dispatch(addTimeOfPressedButton());
   dispatch({ type: ADD_TIMER_TO_QUEUE, payload: timerDelay });
   if (state.timer.queue.length === 0) {
-      dispatch(startTimer(timerDelay));
-    }
+    dispatch(startTimer(timerDelay));
+  }
 };
 
 export const addTimeOfPressedButton = () => {
-    const date = new Date();
-  const timeOfPressedButton = date.toTimeString().split(' ')[0];
-    return { type: ADD_TIME_PRESSED_BUTTON, payload: timeOfPressedButton };
-
+  const date = new Date();
+  const timeOfPressedButton = date.toTimeString().split(" ")[0];
+  return { type: ADD_TIME_PRESSED_BUTTON, payload: timeOfPressedButton };
 };
 
-
-export const startTimer = () => (dispatch, getState) => {
-  const date = new Date();
+export const startTimer = (clearTimeout) => (dispatch, getState) => {
   const state = getState();
+  const date = new Date();
   if (state.timer.queue.length > 0) {
-    const startTime = date.toTimeString().split(' ')[0];
+    const startTime = date.toTimeString().split(" ")[0];
     const buttonNumber = state.timer.queue[0].toString();
     const timerDelay = state.timer.queue[0];
-    setTimeout(() => {
-        dispatch(addCurrentLog({startTime, buttonNumber}));
+    const timerId = setTimeout(() => {
+      dispatch(addCurrentLog({ startTime, buttonNumber }));
       dispatch(removeTimerFromQueue());
-      dispatch(startTimer(timerDelay));
+      dispatch(startTimer());
     }, timerDelay * 1000);
-}
+    dispatch({ type: SET_CURRENT_TIMER, payload: timerId });
+  }
 };
 
 export const removeTimerFromQueue = () => {
-    return { type: REMOVE_TIMER_FROM_QUEUE }
-
+  return { type: REMOVE_TIMER_FROM_QUEUE };
 };
 
 export const addCurrentLog = (data) => {
--- src/store/reducers/timerReducer.js
+++ src/store/reducers/timerReducer.js
@@ -4,13 +4,15 @@ import {
   ADD_CURRENT_LOG,
   ADD_TIME_PRESSED_BUTTON,
   CLEAR_LOGS,
+  SET_CURRENT_TIMER,
 } from "../actions/timerActions";
-import {getItemFromLS, setItemToLS} from '../../utils/localStorage'
+import { getItemFromLS, setItemToLS } from "../../utils/localStorage";
 
 const initialValues = {
   queue: getItemFromLS("queue") || [],
   logs: [],
   timesOfPressedButtons: getItemFromLS("timesOfPressedButtons") || [],
+  currentTimer: "",
 };
 
 const userReducer = (state = initialValues, { type, payload } = {}) => {
@@ -22,7 +24,10 @@ const userReducer = (state = initialValues, { type, payload } = {}) => {
       return { ...state, queue: newQueue };
     }
     case ADD_TIME_PRESSED_BUTTON: {
-      const newTimesOfPressedButtons = [...state.timesOfPressedButtons, payload];
+      const newTimesOfPressedButtons = [
+        ...state.timesOfPressedButtons,
+        payload,
+      ];
       setItemToLS("timesOfPressedButtons", newTimesOfPressedButtons);
       return { ...state, timesOfPressedButtons: newTimesOfPressedButtons };
     }
@@ -34,23 +39,42 @@ const userReducer = (state = initialValues, { type, payload } = {}) => {
     case ADD_CURRENT_LOG: {
       const timeOfPressedButton = state.timesOfPressedButtons[0];
       const date = new Date();
-      const logShowTime = date.toTimeString().split(' ')[0];
-      const timeOfPressedButtonInSec = timeOfPressedButton?.split(':')[0] * 3600 + parseInt(timeOfPressedButton?.split(':')[1]*60) + parseInt(timeOfPressedButton?.split(':')[2]);
-      const logShowTimeInSec = logShowTime.split(':')[0] * 3600 + parseInt(logShowTime.split(':')[1]*60) + parseInt(logShowTime.split(':')[2]);
+      const logShowTime = date.toTimeString().split(" ")[0];
+      const timeOfPressedButtonInSec =
+        timeOfPressedButton?.split(":")[0] * 3600 +
+        parseInt(timeOfPressedButton?.split(":")[1] * 60) +
+        parseInt(timeOfPressedButton?.split(":")[2]);
+      const logShowTimeInSec =
+        logShowTime.split(":")[0] * 3600 +
+        parseInt(logShowTime.split(":")[1] * 60) +
+        parseInt(logShowTime.split(":")[2]);
       const difference = logShowTimeInSec - timeOfPressedButtonInSec;
-      const logObj = {button: payload.buttonNumber, timeOfPressedButton: timeOfPressedButton, endTime: logShowTime, difference: difference};
+      const logObj = {
+        button: payload.buttonNumber,
+        timeOfPressedButton: timeOfPressedButton,
+        endTime: logShowTime,
+        difference: difference,
+      };
       const newLogs = [...state.logs, logObj];
       const newTimesOfPressedButtons = state.timesOfPressedButtons.slice(1);
       setItemToLS("timesOfPressedButtons", newTimesOfPressedButtons);
-      return { ...state, logs: newLogs, timesOfPressedButtons: newTimesOfPressedButtons };
+      return {
+        ...state,
+        logs: newLogs,
+        timesOfPressedButtons: newTimesOfPressedButtons,
+      };
     }
-    
+
     case CLEAR_LOGS: {
       setItemToLS("queue", null);
       setItemToLS("timesOfPressedButtons", null);
+      clearTimeout(state.currentTimer);
       return { ...state, logs: [], timesOfPressedButtons: [], queue: [] };
     }
-    
+    case SET_CURRENT_TIMER: {
+      return { ...state, currentTimer: payload };
+    }
+
     default:
       return state;
   }
